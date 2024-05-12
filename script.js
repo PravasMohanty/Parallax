0da35b1d49f6e92e930310122ac85ab7abd5da2f
Pravas Mohanty <pravasmohanty1000@gmail.com>
2024-05-12 11:08:31 +0530

    Adding Updated Files

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll',() =>{
+    let value = window.scrollY;
+
+    text.style.marginTop = value*2.5+'px';
+    leaf.style.top = -2.5*value+'px';
+    leaf.style.left = 1.5*value+'px';
+    hill4.style.left = value*1.5 + 'px';
+    hill5.style.left = -1.5*value* + 'px';
+})
